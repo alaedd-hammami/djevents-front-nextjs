8e6b485ddd582f88e1bf3b8069673527dde1194d
unknown <alaedd@gmail.com>
2021-07-09 00:11:52 +0100

    Layout, Header and Footer

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+export default function AboutPage() {
+  return (
+    <Layout title="About DJ Events">
+      <div>
+        <h1>About</h1>
+        <p>This is an app to find the latest DJ and othermusical events</p>
+        <p>Version: 1.0.0</p>
+      </div>
+    </Layout>
+  )
+}
